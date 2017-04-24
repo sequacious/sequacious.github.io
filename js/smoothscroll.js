824b763c77860bf0ef596166b17ef21547612ef0
Laryssa Caye Bates <Laryssa@13-73-203.client.wireless.msu.edu>
2017-04-24 13:43:16 -0400

    uploaded site materials

---
+++ js/smoothscroll.js
@@ -0,0 +1,23 @@
+$("#scrollToSection01").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section-01").offset().top
+    }, 2000);
+});
+
+$("#scrollToSection02").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section-02").offset().top
+    }, 2000);
+});
+
+$("#scrollToSection03").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section-03").offset().top
+    }, 2000);
+});
+
+$("#scrollToSection04").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#section-04").offset().top
+    }, 2000);
+});
